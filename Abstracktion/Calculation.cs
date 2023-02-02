6edd79877f8ba41f88e5851129c6378620562903
Alaskarli <nazrinna@code.edu.az>
2023-02-01 17:58:18 -0800

    abstraction

---
+++ Abstracktion/Calculation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracktion
+{
+    internal abstract class Calculation
+    {
+        public abstract int Operate(int x,int y);
+    }
+
+
+
+    internal sealed class Addition:Calculation
+    {
+        public override int Operate(int x,int y)
+        {
+            int res = x + y;
+            Console.WriteLine($"Addition:First number={x} Second number={y} Result={res}");
+            return x + y;
+        }
+    }
+    internal sealed class Substraction:Calculation
+    {
+        public override int Operate(int x, int y)
+        {
+            int res = x - y;
+            Console.WriteLine($"Substraction:First number={x} Second number={y} Result={res}");
+            return x - y;
+        }
+    } 
+    internal sealed class Multiplication:Calculation
+    {
+        public override int Operate(int x, int y)
+        {
+            int res = x * y;
+            Console.WriteLine($"Multiplication:First number={x} Second number={y} Result={res}");
+            return x * y;
+        }
+    }
+    internal sealed class Division:Calculation
+    {
+        public override int Operate(int x, int y)
+        {
+            int res = x / y;
+            Console.WriteLine($"Division:First number={x} Second number={y} Result={res}");
+            return x / y;
+        }
+    }
+
+}
---
+++ Abstracktion/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Abstracktion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition addition= new Addition();
+            addition.Operate(5,2);
+
+
+
+            Substraction substraction= new Substraction();
+            substraction.Operate(5, 2);
+
+
+
+
+            Multiplication multiplication= new Multiplication();    
+            multiplication.Operate(5, 2);   
+
+
+
+
+            Division division= new Division();  
+            division.Operate(5, 2);
+        }
+    }
+}
